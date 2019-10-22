e678a326ad34bf35cdc321a13329b4d0ec762533
Ttayu <ykisoccer.pg@gmail.com>
2019-10-23 04:04:32 +0900

    Add key remap configuration for windows.

---
+++ .config/key_remap/windows/config.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+import pyauto
+from keyhac import *
+
+
+def configure(keymap):
+    # --------------------------------------------------------------------
+    # Customizing the display
+
+    # Font
+    keymap.setFont("MS Gothic", 12)
+
+    # Theme
+    keymap.setTheme("black")
+
+    # --------------------------------------------------------------------
+
+    keymap_global = keymap.defineWindowKeymap()
+
+    # SandS
+    keymap.replaceKey("Space", "LShift")
+    keymap_global["O-LShift"] = "Space"
+
+    # ctrl and escape
+    keymap_global["O-LCtrl"] = "Esc"
+
+    # Ctrl + 数値 で Fn
+    keymap_global["C-1"] = "F1"
+    keymap_global["C-2"] = "F2"
+    keymap_global["C-3"] = "F3"
+    keymap_global["C-4"] = "F4"
+    keymap_global["C-5"] = "F5"
+    keymap_global["C-6"] = "F6"
+    keymap_global["C-7"] = "F7"
+    keymap_global["C-8"] = "F8"
+    keymap_global["C-9"] = "F9"
+    keymap_global["C-0"] = "F10"
+    keymap_global["C-Minus"] = "F11"
+    keymap_global["C-Caret"] = "F12"
