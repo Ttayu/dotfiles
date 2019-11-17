9852c182d52a45c69b42cc6cf40a0cb2afd0897d
Ttayu <32290119+Ttayu@users.noreply.github.com>
2019-11-17 10:27:30 +0900

    Fix xkeysnail configuration.

--- .config/key_remap/linux/start_xkeysnail.sh
+++ .config/key_remap/linux/start_xkeysnail.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 if [ -x /usr/local/bin/xkeysnail ]; then
     xhost +SI:localuser:root
-    sudo -u root DISPLAY=:0 /usr/local/bin/xkeysnail /etc/opt/xkeysnail/config.py &
+    sudo -u root DISPLAY=:0.0 /usr/local/bin/xkeysnail /etc/opt/xkeysnail/config.py
 fi
