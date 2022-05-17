a9ee6327fc32b7d2cb773baf0188013ad2a062ca
Ttayu <32290119+Ttayu@users.noreply.github.com>
2022-05-17 15:21:52 +0900

    Update lsp

--- .config/nvim/plugins/lsp.lua
+++ .config/nvim/plugins/lsp.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_keymap('n', '<Leader>l', '[lsp]', { noremap = false })
 vim.api.nvim_set_keymap('n', '[lsp]a', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '[lsp]c', ':lua vim.lsp.buf.declaration()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '[lsp]d', ':lua vim.lsp.buf.definition()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '[lsp]f', ':lua vim.lsp.buf.formatting()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '[lsp]f', ':lua vim.lsp.buf.format{ async=true }<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '[lsp]h', ':lua vim.lsp.buf.hover()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '[lsp]i', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '[lsp]x', ':lua vim.lsp.buf.references()<CR>', { noremap = true })
@@ -50,85 +50,37 @@ function PrintDiagnostics(opts, bufnr, line_nr)
 end
 vim.cmd [[ autocmd CursorHold * lua PrintDiagnostics() ]]
 
-local lsp_installer = require("nvim-lsp-installer")
+-- nvim-lsp-installer
+require"nvim-lsp-installer".setup{}
 local lspconfig = require "lspconfig"
-local configs = require "lspconfig/configs"
-local servers = require "nvim-lsp-installer.servers"
-local server = require "nvim-lsp-installer.server"
-local shell = require "nvim-lsp-installer.installers.shell"
 
--- julia
-local server_name = "julials"
-local root_dir = server.get_server_root_path(server_name)
--- -- 1. (optional, only if lspconfig doesn't already support the server)
--- --    Create server entry in lspconfig
-configs[server_name] = {
-    default_config = {
-        filetypes = { "julia" },
-        root_dir = lspconfig.util.root_pattern(".git", "project.toml"),
-    },
-}
--- require'lspconfig'.julials.setup{}
-
-local julials = server.Server:new {
-    name = server_name,
-    root_dir = root_dir,
-    languages = { "julia" },
-    installer = shell.bash [[
-      julia -e 'using Pkg; Pkg.add("LanguageServer")'
-    ]],
-    default_options = {
-        cmd = { "julia", "-e", "using LanguageServer, LanguageServer.SymbolServer; runserver()"
-        },
-    },
-}
-
--- 3. (optional, recommended) Register your server with nvim-lsp-installer.
---    This makes it available via other APIs (e.g., :LspInstall, lsp_installer.get_available_servers()).
-servers.register(julials)
-
--- r-languageserver
-server_name = "r_language_server"
-root_dir = server.get_server_root_path(server_name)
-
-require'lspconfig'.r_language_server.setup{}
-
-local r_ls = server.Server:new {
-    name = server_name,
-    root_dir = root_dir,
-    languages = { "r", "rmd" },
-    installer = shell.bash [[
-      R -e 'install.packages("languageserver", repos = "https://cran.r-project.org")'
-    ]],
-    default_options = {
-        cmd = { "R", "--slave", "-e", "languageserver::run()"
-        },
-    },
-}
-servers.register(r_ls)
-
--- python
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    if server.name == "pylsp" then
-        opts.diagnostics = {enable = true}
-        opts.completion = {ignorePatterns = {"^_{1,3}$|^_[^_].*$|^__.*(?<!__)$"} }
-        opts.settings = {
-          pylsp = {
-            configurationSources = {"flake8"},
-            plugins = {
-              pyflakes = {enabled = false},
-              pycodestyle = {enabled = false},
-              mccabe = {enabled = false},
-              flake8 = {enabled = true},
-              autopep8 = {enabled = false},
-              pyls_flake8 = {enabled = false},
-              pylsp_black = {enabled = true},
-              pyls_isort = {enabled = true},
-              pylsp_mypy = {enabled = true, overrides = {true, "--ignore-missing-imports"}},
-            }
-          }
-        }
-    end
-    server:setup(opts)
-end)
+lspconfig.julials.setup({})
+lspconfig.r_language_server.setup({})
+lspconfig.pylsp.setup({
+  root_dir = function(fname)
+    local root_files = {
+      "pyproject.toml",
+      "setup.py",
+      "setup.cfg",
+      "requirements.txt",
+      "Pipfile",
+    }
+    return lspconfig.util.root_pattern(unpack(root_files))(fname) or lspconfig.util.find_git_ancestor(fname)
+  end,
+  settings = {
+    pylsp = {
+      configurationSources = {"flake8"},
+      plugins = {
+        pyflakes = {enabled = false},
+        pycodestyle = {enabled = false},
+        mccabe = {enabled = false},
+        flake8 = {enabled = true},
+        autopep8 = {enabled = false},
+        pyls_flake8 = {enabled = false},
+        pylsp_black = {enabled = true},
+        pyls_isort = {enabled = true},
+        pylsp_mypy = {enabled = true, overrides = {true, "--ignore-missing-imports"}},
+      }
+    }
+  }
+})
