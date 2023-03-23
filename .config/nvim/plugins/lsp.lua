acf7327d0d8a93936d27f47a1c075de0bc135e15
Ttayu <32290119+Ttayu@users.noreply.github.com>
2023-03-23 18:51:09 +0900

    refactor

--- .config/nvim/plugins/lsp.lua
+++ .config/nvim/plugins/lsp.lua
@@ -32,12 +32,10 @@ vim.diagnostic.config({
 	float = {
 		source = "always", -- Or "if_many"
 	},
+	virtual_text = false,
 })
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	update_in_insert = false,
-	virtual_text = false,
-})
-vim.diagnostic.config({
 	virtual_text = false,
 })
 
