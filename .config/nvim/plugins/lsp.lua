6c81b72c1ecf16b8286a8010e135580f4789f91b
Ttayu <32290119+Ttayu@users.noreply.github.com>
2023-10-11 15:47:25 +0900

    style: apply formatter

--- .config/nvim/plugins/lsp.lua
+++ .config/nvim/plugins/lsp.lua
@@ -1,7 +1,7 @@
 require("lspsaga").setup({
-	diagnostic = {
-		on_insert = false,
-	},
+  diagnostic = {
+    on_insert = false,
+  },
 })
 vim.api.nvim_set_keymap("n", "[lsp]", "<Nop>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>l", "[lsp]", { noremap = false })
@@ -10,10 +10,10 @@ vim.api.nvim_set_keymap("n", "[lsp]c", ":lua vim.lsp.buf.declaration()<CR>", { n
 vim.api.nvim_set_keymap("n", "[lsp]dp", ":Lspsaga peek_definition<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[lsp]dd", ":Lspsaga goto_definition<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap(
-	"n",
-	"[lsp]f",
-	":lua vim.lsp.buf.format{timeout_ms = 5000, async = true}<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[lsp]f",
+  ":lua vim.lsp.buf.format{timeout_ms = 5000, async = true}<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "[lsp]h", ":Lspsaga hover_doc<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[lsp]i", ":lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
@@ -29,14 +29,14 @@ vim.api.nvim_set_keymap("i", "<C-s>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>
 
 -- general settings
 vim.diagnostic.config({
-	float = {
-		source = "always", -- Or "if_many"
-	},
-	virtual_text = false,
+  float = {
+    source = "always", -- Or "if_many"
+  },
+  virtual_text = false,
 })
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	update_in_insert = false,
-	virtual_text = false,
+  update_in_insert = false,
+  virtual_text = false,
   underline = false,
 })
 
@@ -46,28 +46,28 @@ vim.o.updatetime = 100
 -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 function PrintDiagnostics(opts, bufnr, line_nr)
-	opts = opts or {}
+  opts = opts or {}
 
-	bufnr = bufnr or 0
-	line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-	local line_diagnostics = vim.diagnostic.get(bufnr, { lnum = line_nr })
-	if vim.tbl_isempty(line_diagnostics) then
-		return
-	end
+  bufnr = bufnr or 0
+  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+  local line_diagnostics = vim.diagnostic.get(bufnr, { lnum = line_nr })
+  if vim.tbl_isempty(line_diagnostics) then
+    return
+  end
 
-	for _, diagnostic in ipairs(line_diagnostics) do
-		local output = string.format(
-			"[%s] %s [%s]",
-			diagnostic.source or "",
-			diagnostic.message or "",
-			vim.diagnostic.severity[diagnostic.severity] or ""
-		)
-		if #output > 140 then
-			output = string.sub(output, 0, 140) .. "..."
-		end
-		vim.api.nvim_echo({ { output } }, false, {})
-		break
-	end
+  for _, diagnostic in ipairs(line_diagnostics) do
+    local output = string.format(
+      "[%s] %s [%s]",
+      diagnostic.source or "",
+      diagnostic.message or "",
+      vim.diagnostic.severity[diagnostic.severity] or ""
+    )
+    if #output > 140 then
+      output = string.sub(output, 0, 140) .. "..."
+    end
+    vim.api.nvim_echo({ { output } }, false, {})
+    break
+  end
 end
 
 vim.cmd([[ autocmd CursorHold * lua PrintDiagnostics() ]])
@@ -79,15 +79,16 @@ local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup({})
 mason_lspconfig.setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({})
-	end,
+  function(server_name)
+    lspconfig[server_name].setup({})
+  end,
 })
 
 local mason_null_ls = require("mason-null-ls")
 mason_null_ls.setup({
-	automatic_setup = true,
-	handlers = {},
+  ensure_installed = {},
+  automatic_installation = false,
+  handlers = {},
 })
 require("null-ls").setup({})
 
@@ -97,82 +98,82 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 })
 -- }}}
 
 -- python {{{
 lspconfig.pylsp.setup({
-	root_dir = function(fname)
-		local root_files = {
-			"pyproject.toml",
-			"setup.py",
-			"setup.cfg",
-			"requirements.txt",
-			"Pipfile",
-		}
-		return lspconfig.util.root_pattern(unpack(root_files))(fname) or lspconfig.util.find_git_ancestor(fname)
-	end,
-	settings = {
-		pylsp = {
-			configurationSources = { "flake8" },
-			plugins = {
-				pyflakes = { enabled = false },
-				pycodestyle = { enabled = false },
-				mccabe = { enabled = false },
-				flake8 = { enabled = false },
-				autopep8 = { enabled = false },
-				pyls_flake8 = { enabled = false },
-				pylsp_black = { enabled = true },
-				pyls_isort = { enabled = true },
-				yapf = { enabled = false },
-				pylsp_mypy = { enabled = true, overrides = { true, "--ignore-missing-imports" } },
-				ruff = { enabled = true },
-			},
-		},
-	},
+  root_dir = function(fname)
+    local root_files = {
+      "pyproject.toml",
+      "setup.py",
+      "setup.cfg",
+      "requirements.txt",
+      "Pipfile",
+    }
+    return lspconfig.util.root_pattern(unpack(root_files))(fname) or lspconfig.util.find_git_ancestor(fname)
+  end,
+  settings = {
+    pylsp = {
+      configurationSources = { "flake8" },
+      plugins = {
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        mccabe = { enabled = false },
+        flake8 = { enabled = false },
+        autopep8 = { enabled = false },
+        pyls_flake8 = { enabled = false },
+        pylsp_black = { enabled = true },
+        pyls_isort = { enabled = true },
+        yapf = { enabled = false },
+        pylsp_mypy = { enabled = true, overrides = { true, "--ignore-missing-imports" } },
+        ruff = { enabled = true },
+      },
+    },
+  },
 })
 -- }}}
 
 -- typescript {{{
 lspconfig.denols.setup({
-	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "denops"),
-	init_options = {
-		lint = true,
-		unstable = true,
-		suggest = {
-			imports = {
-				hosts = {
-					["https://deno.land"] = true,
-					["https://cdn.nest.land"] = true,
-					["https://crux.land"] = true,
-				},
-			},
-		},
-	},
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "denops"),
+  init_options = {
+    lint = true,
+    unstable = true,
+    suggest = {
+      imports = {
+        hosts = {
+          ["https://deno.land"] = true,
+          ["https://cdn.nest.land"] = true,
+          ["https://crux.land"] = true,
+        },
+      },
+    },
+  },
 })
 
 lspconfig.tsserver.setup({
-	single_file_support = false,
-	root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false,
+  root_dir = lspconfig.util.root_pattern("package.json"),
 })
 -- }}}
