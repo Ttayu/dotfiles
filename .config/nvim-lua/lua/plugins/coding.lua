69126712cd857353f4b3440fe05b32f449c7025f
Ttayu <32290119+Ttayu@users.noreply.github.com>
2025-06-27 13:43:47 +0900

    chore(plugins): move hop.nvim plugin configuration

--- .config/nvim-lua/lua/plugins/coding.lua
+++ .config/nvim-lua/lua/plugins/coding.lua
@@ -84,13 +84,4 @@ return {
     },
     opts_extend = { "sources.default" }
   },
-  {
-    "smoka7/hop.nvim",
-    event = "VeryLazy",
-    config = true,
-    keys = {
-      { "ff", "<CMD>HopWord<CR>",      desc = "HopWord" },
-      { "fl", "<CMD>HopLineStart<CR>", desc = "HopLineStart" },
-    }
-  }
 }
--- .config/nvim-lua/lua/plugins/editor.lua
+++ .config/nvim-lua/lua/plugins/editor.lua
@@ -25,5 +25,14 @@ return {
         desc = "Buffer Local Keymaps (which-key)",
       },
     }
+  },
+  {
+    "smoka7/hop.nvim",
+    event = "VeryLazy",
+    config = true,
+    keys = {
+      { "ff", "<CMD>HopWord<CR>",      desc = "HopWord" },
+      { "fl", "<CMD>HopLineStart<CR>", desc = "HopLineStart" },
+    }
   }
 }
