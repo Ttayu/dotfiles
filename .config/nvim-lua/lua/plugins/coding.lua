17552fd873da2f4afbaa06ba9ceccaf3c2de9cd6
Ttayu <32290119+Ttayu@users.noreply.github.com>
2025-06-27 13:38:35 +0900

    style(coding): fix formatting in coding.lua

--- .config/nvim-lua/lua/plugins/coding.lua
+++ .config/nvim-lua/lua/plugins/coding.lua
@@ -6,7 +6,7 @@ return {
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "snacks.nvim",        words = { "Snacks" } },
       },
     },
   },
