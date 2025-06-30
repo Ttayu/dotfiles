4634a9fa31ca77461264b47f93a849dd08d9a406
Ttayu <32290119+Ttayu@users.noreply.github.com>
2025-06-30 10:37:36 +0900

    fix(lazy): prevent unnecessary notification popups

--- .config/nvim-lua/lua/config/lazy.lua
+++ .config/nvim-lua/lua/config/lazy.lua
@@ -22,5 +22,10 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
