77803bd715ad9a0ed2dc91fe3ae768d94b67038d
Ttayu <32290119+Ttayu@users.noreply.github.com>
2025-07-04 09:50:26 +0900

    fix(coding): prevent duplicate completions in blink.cmp

--- .config/nvim-lua/lua/plugins/coding.lua
+++ .config/nvim-lua/lua/plugins/coding.lua
@@ -75,6 +75,7 @@ return {
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        priority = { "cmdline-history", "cmdline", "lsp", "path", "snippets", "buffer" },
         providers = {
           lazydev = {
             name = "LazyDev",
@@ -96,7 +97,25 @@ return {
       -- See the fuzzy documentation for more information
       fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" },
+    config = function(_, opts)
+      -- Duplicate completions: https://github.com/Saghen/blink.cmp/issues/1222
+      local original = require("blink.cmp.completion.list").show
+      ---@diagnostic disable-next-line: duplicate-set-field
+      require("blink.cmp.completion.list").show = function(ctx, items_by_source)
+        local seen = {}
+        local function filter(item)
+          if seen[item.label] then return false end
+          seen[item.label] = true
+          return true
+        end
+        for id in vim.iter(opts.sources.priority) do
+          items_by_source[id] = items_by_source[id] and vim.iter(items_by_source[id]):filter(filter):totable()
+        end
+        return original(ctx, items_by_source)
+      end
+      require("blink.cmp").setup(opts)
+    end
   },
   {
     "echasnovski/mini.pairs",
