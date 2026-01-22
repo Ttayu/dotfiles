7de21874529f7a7c99a75cfdd30864ce96d1a61b
Ttayu <32290119+Ttayu@users.noreply.github.com>
2026-01-22 17:13:34 +0900

    feat(treesitter): add textobjects plugin and keymaps for assignments, functions, and classes
    - Add nvim-treesitter-textobjects dependency
    - Configure select keymaps for assignment, function, and class
    - Enable textobjects selection with lookahead

--- .config/nvim-lua/lua/plugins/treesitter.lua
+++ .config/nvim-lua/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     dependencies = {
       "RRethy/nvim-treesitter-endwise",
       "RRethy/nvim-treesitter-textsubjects",
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     main = "nvim-treesitter.configs",
     opts = {
@@ -21,7 +22,23 @@ return {
           [";"] = "textsubjects-container-outer",
           ["i;"] = "textsubjects-container-inner",
         }
-      }
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["ao"] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            ["ai"] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            ["ah"] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            ["al"] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+            ["af"] = { query = "@function.outer", desc = "Select outerpart of a function region." },
+            ["if"] = { query = "@function.inner", desc = "Select outerpart of a function region." },
+            ["ac"] = { query = "@class.outer", desc = "Select outerpart of a class region." },
+            ["ic"] = { query = "@class.inner", desc = "Select outerpart of a class region." },
+          }
+        }
+      },
     }
   },
   {
