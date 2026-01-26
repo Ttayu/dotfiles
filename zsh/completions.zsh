2c557209d25432e4c89954e2176000aab4746190
Ttayu <32290119+Ttayu@users.noreply.github.com>
2026-01-26 12:35:17 +0900

    feat(zsh): add oil-ssh support to file completion
    - Override _vim_files to include oil-ssh URLs
    - Rewrite _urls to recognize oil-ssh scheme
    - Update LS_COLORS for oil-ssh

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -24,3 +24,26 @@ zmodload zsh/complist
 LS_COLORS="${LS_COLORS}:ow=01;34"; export LS_COLORS
 _ls_colors="ow=01;34"
 zstyle ':completion:*:default' list-colors "${(s.:.)_ls_colors}"
+
+# Override _vim_files to add oil-ssh
+_vim_files() {
+  case $PREFIX in
+    (+*) _files -P './' $* && return 0 ;;
+    ((scp|http(|s)|(|s)ftp|oil-ssh):*) _urls ;;
+    (*) _files $* ;;
+  esac
+  case $PREFIX in
+    (+) _message -e 'start at a given line (default: end of file)' ;;
+    (+<1->) _message -e 'line number' ;;
+  esac
+}
+
+# Rewrite _urls (close scope with anonymous function)
+() {
+  autoload +X _urls 2>/dev/null
+  local body="$functions[_urls]"
+  # 正確なパターンのみ置換 (前後の文字で区切る)
+  body="${body//\$scheme = \(scp\|sftp\)/\$scheme = (scp|sftp|oil-ssh)}"
+  body="${body//\(http\(\|s\)\|\(\|s\)ftp\|scp\|gopher\)/(http(|s)|(|s)ftp|scp|gopher|oil-ssh)}"
+  functions[_urls]="$body"
+}
